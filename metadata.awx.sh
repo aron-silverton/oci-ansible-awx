fd3eccb3d5565630df619b8d0d7817d11ae5a5fe
Aron Silverton <aron.silverton@oracle.com>
2021-02-17 12:09:41 -0600

    Install proxyt transparent proxy
    This is required for the linuxandvirtualization and osdevelopment
    tenancies because they cannot reach the Internet without a proxy.
    
    Signed-off-by: Aron Silverton <aron.silverton@oracle.com>

--- metadata.awx.sh
+++ metadata.awx.sh
@@ -11,6 +11,15 @@ systemctl enable --now yum-cron.service
 
 uptrack-upgrade -y --all
 
+# Install the proxyt transparent proxy client
+wget http://kernel.us.oracle.com/~jhaxby/proxyt/repo/7/x86_64/proxyt-4.1.1-1.el7.x86_64.rpm
+yum install ./proxyt-4.1.1-1.el7.x86_64.rpm -y
+wget http://kernel.us.oracle.com/~jhaxby/proxyt/proxyt.repo
+install -o root -g root -m 0644 proxyt.repo /etc/yum.repos.d/proxyt.repo
+yum --disablerepo=* --enablerepo=proxyt makecache
+yum update proxyt -y
+#rm -f proxyt*
+
 for service in http https
 do
   # XXX Prevent terraform from prasing service variable by not using curly brace
